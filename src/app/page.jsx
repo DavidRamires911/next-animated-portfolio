6377ea45330ca5bc8f347b4666187fd1fc7589e5
DavidCorreia911 <davidcorreia911@gmail.com>
2024-03-04 14:11:48 +0000

    back to original

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,34 +20,30 @@ const Homepage = () => {
             alt="hero"
             className="object-contain "
             fill
-            priority          />
+            priority
+          />
         </div>
-        ;{/* text container*/}
+        {/* text container*/}
         <div className="h-1/2 lg:h-full lg:w-1/2  flex flex-col gap-8 items-center justify-center">
           {/* Title */}
           <h1 className="text-4xl md:text-6xl  font-bold">
-            {" "}
-            Shaping the Future of Digital Interactions{" "}
+            Shaping the Future of Digital Interactions
           </h1>
           {/* desc */}
           <p className="md:text-xl">
-            As a highly motivated fullstack web developer, I bring over a year of
-            freelance experience and dedicated constant formation in both
+            As a highly motivated fullstack web developer, I bring over a year
+            of freelance experience and dedicated constant formation in both
             front-end and back-end technologies. With strong resilience and
-            diverse background , i thrive in collaborative settings and adapt
-            swiftly to new challenges. 
+            diverse background  i thrive in collaborative settings and adapt
+            swiftly to new challenges.
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
-            <button  className="hover:bg-gray-800 hover:scale-105 p-4 rounded-lg ring-1 ring-black bg-black text-white">
-            <Link href="/portfolio">
-              View My Work
-              </Link>
+            <button className="hover:bg-gray-800 hover:scale-105 p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              <Link href="/portfolio">View My Work</Link>
             </button>
             <button className="hover:bg-gray-100 hover:scale-105 p-4 rounded-lg ring-1 ring-black ">
-            <Link href="/contact">
-              Contact Me
-              </Link>
+              <Link href="/contact">Contact Me</Link>
             </button>
           </div>
         </div>
